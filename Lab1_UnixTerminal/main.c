9a7c2847b7fea5c422e5475a2f4325198febb9ab
alkhalifas <salehesam@gmail.com>
2022-01-23 12:33:43 -0500

    Fixed issue in lab1 main.c, modified the readme with my name

--- Lab1_UnixTerminal/main.c
+++ Lab1_UnixTerminal/main.c
@@ -1,1 +1,24 @@
-// Add your program here!
+#include <stdio.h>
+
+
+double power(double base, double n) {
+
+	double answer = 1;
+	int i;
+
+	for (i = 0; i<n; i++) { 
+		answer *= base;
+	}
+	return answer;
+}
+
+
+int main() {
+	
+	int i;
+	for (i = 0; i < 11; i++) 
+		printf("2^%d= %f\n", i, power(2,i));
+
+	return 0;	
+}
+
