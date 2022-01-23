a5c31174e09f07273d28fa2013e7f7386db86d40
alkhalifas <salehesam@gmail.com>
2022-01-23 13:59:53 -0500

    Assignment 1: Added random and user input

--- Assignment1_GuessingGame/part1/main.c
+++ Assignment1_GuessingGame/part1/main.c
@@ -1,1 +1,34 @@
-// Add your program here!
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	// Generate pseudo-random number using rand()
+	srand(time(NULL));
+	int random = rand() %10 + 1;
+	
+	// GUI Printing
+	printf("===============================\n");
+	printf(" CPU Says: Pick a Number 1-10: \n");
+	printf("===============================\n");
+
+	// Getting User Input
+	int userGuess;
+	
+	printf("Make a guess: ");
+	scanf("%d", &userGuess);	
+	printf("User Guess: %d\n", userGuess);
+	printf("CPUs Guess: %d\n", random);
+
+	// Comparing the Values:
+	
+	if (userGuess == random) {
+		printf("---- Correct!\n");
+	} else {
+		printf("---- Incorrect!\n");
+	}
+
+	return 0;
+}
