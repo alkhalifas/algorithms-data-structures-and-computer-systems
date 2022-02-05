eb33bab04ec648b041827d9fbb25cf1cd0fc907a
alkhalifas <salehesam@gmail.com>
2022-02-05 16:44:53 -0500

    Worked on CStrings using Mike's video, working on Lab 3

--- Lab3_CStrings/part1/mystring.c
+++ Lab3_CStrings/part1/mystring.c
@@ -4,18 +4,32 @@
 #include "mystring.h"
 
 // search: 'man strlen' to see what function should do
+//
+//
+//
 size_t my_strlen(const char* input){
-    // IMPLEMENT
-    return 0;   
+    int length = 0;
+    while(input[length] != '\0';
+        length++;
+    }
+    return length;   
 }
 
 // search: 'man strcmp' to see what function should do
+//
+//
+//
+//
 int my_strcmp(const char* src1, const char* src2){
     // IMPLEMENT
     return 0;   
 }
 
 // search: 'man strchr' to see what function should do
+//
+//
+//
+//
 char* my_strchr(const char* src1, int c){
     // IMPLEMENT
     return NULL;   
@@ -27,6 +41,43 @@ char* my_strchr(const char* src1, int c){
 //       I would like you to return a 'heap allocated' string
 //       that appends both of src1 and src2 into a new buffer
 char* my_string_append(const char* src1, const char* src2){
-    // IMPLEMENT
-    return NULL;   
+    // Compute the lengths of the two strings
+    size_t src1Length = StringLength(src1);
+    size_t src2Length = StringLength(src2);
+
+    // Set storage for the new string
+    char* newString = (char*)malloc(sizeof(char)*(src1Length+src2Length+1));
+
+    // Copy first string into new string
+    int i = 0;
+    while(i<src1Length){
+        newString[i] = src1[i];
+        i++;
+    }
+
+    // Copy second string into new string
+    int j = 0;
+    while(j<src2Length){
+        newString[src1Length+j] = src2[j];
+        j++;
+    }
+
+    // Add null terminator
+    newString[src1Length+src2Length] = '\0';
+
+    return newString;   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
