4eee688df539d40689422b7adae6d52e0c6ea484
alkhalifas <salehesam@gmail.com>
2022-02-07 19:46:24 -0500

    create_queue, queue_full, and queue_empty all working

--- Assignment3_Queues/part1/queue_t.c
+++ Assignment3_Queues/part1/queue_t.c
@@ -1,18 +1,19 @@
 #include "queue_t.h"
 #include <stdlib.h> // malloc/free
+#include <stdio.h>
 
 // Create a queue
 queue_t* create_queue(unsigned int _capacity){
     
     // Create a new queue and set size using malloc
-    queue_t* newQueue = (queue_t*)malloc(size_of(queue_t));
+    queue_t* newQueue = (queue_t*)malloc(sizeof(queue_t));
 
     // Set the back, front, size, capacity, and data
     newQueue->back = _capacity - 1;
     newQueue->front = 0;
     newQueue->capacity = _capacity;
     newQueue->size = 0;
-    newQueue->data = 0;
+    newQueue->data = (int*)malloc(_capacity*sizeof(int));;
 
     return newQueue; 
 }
@@ -60,4 +61,29 @@ unsigned int queue_size(queue_t* q){
 // Free queue
 void free_queue(queue_t* q){
 
+}
+
+
+
+
+int main() {
+
+    queue_t* test1 = create_queue(1);
+    printf("########################################\n");
+    printf("Attempting to add %d\n",1);
+    printf("Printing test1->size = %d\n", test1->size);
+    printf("Printing test1->capacity = %d\n", test1->capacity);
+    printf("Printing test1->data = %d\n", test1->data);
+    printf("Printing test1->front = %d\n", test1->front);
+    printf("Printing test1->back = %d\n", test1->back);
+    printf("----------------------------------------\n");
+    printf("Printing queue_empty(test1) = %d\n", queue_empty(test1));
+    printf("Printing queue_full(test1) = %d\n", queue_full(test1));
+
+
+    //queue_enqueue(test1,1);	
+    //printf("Removing: %d\n",queue_dequeue(test1));	
+    //free_queue(test1);
+
+    return 0;
 }
