c7664c7a697141b26d577b181fba4425784803c4
alkhalifas <salehesam@gmail.com>
2022-02-10 19:57:05 -0500

    Lab4-Part2 completed

--- Lab4_Assembly/part2/hello.s
+++ Lab4_Assembly/part2/hello.s
@@ -1,2 +1,28 @@
-# Your assembly code here
-# Type out the assembly--don't just copy and paste!
+# This is a file that is an example of an x86-64 processor using ATT style syntax
+.global _start
+
+.text
+
+_start:
+      mov $1, %rax
+      mov $1, %rdi
+      mov $message, %rsi
+      mov $13, %rdx
+      syscall
+
+      mov $60, %rax
+      xor %rdi, %rdi
+      syscall
+
+message:
+      .ascii "Hello, World\n"
+
+
+
+
+
+
+
+
+
+
