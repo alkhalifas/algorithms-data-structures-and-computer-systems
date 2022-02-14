464e434458f846fc846c03d5385671b15150172f
alkhalifas <salehesam@gmail.com>
2022-02-13 20:02:45 -0500

    Lab4-Part3-Functions Code Added

--- Lab4_Assembly/part3/add.c
+++ Lab4_Assembly/part3/add.c
@@ -1,1 +1,21 @@
 // Write a C program that calls an add function(long add(long a, long b).
+#include <stdio.h>
+#include <stdlib.h>
+
+
+long add(long a, long b) {
+    return a + b;
+}
+
+
+int main() {
+
+    long x = 4;
+    long y = 6;
+
+    long mySum = add(x, y);
+
+    printf("%d + %d = %d\n", x, y, mySum);
+
+    return 0;
+}
