18c5d1054e12e0cd39fd5b1bfc0df974c282c5b7
alkhalifas <salehesam@gmail.com>
2022-02-13 19:27:47 -0500

    Lab4-Part3-ForLoop Code Added

--- Lab4_Assembly/part3/for.c
+++ Lab4_Assembly/part3/for.c
@@ -1,1 +1,13 @@
 // Write a C program using a for-loop that counts to 5.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("Number is = %d\n", i);
+    }
+
+     return 0;
+}
