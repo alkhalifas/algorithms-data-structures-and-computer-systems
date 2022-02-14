40e832463e1b6d596ba27bcaf6553ac11a3cffec
alkhalifas <salehesam@gmail.com>
2022-02-13 19:14:40 -0500

    Lab4-Part3-Goto Code Added

--- Lab4_Assembly/part3/goto.c
+++ Lab4_Assembly/part3/goto.c
@@ -1,1 +1,29 @@
 // Write a C program using the goto command and a label.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+    
+    int value = 15;
+
+    if (value % 2 == 0) {
+        goto even;
+    } else {
+        goto odd;
+    }
+
+    even:
+        printf("I am EVEN! -> %d\n", value);
+
+
+        return 0;
+    odd:
+        printf("I am ODD! -> %d\n", value);
+
+
+
+    return 0;
+}
---
+++ Lab4_Assembly/part3/goto.s
@@ -0,0 +1,50 @@
+	.file	"goto.c"
+	.section	.rodata
+.LC0:
+	.string	"I am EVEN! -> %d\n"
+.LC1:
+	.string	"I am ODD! -> %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$15, -4(%rbp)
+	movl	-4(%rbp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
+	jne	.L2
+	nop
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	jmp	.L5
+.L2:
+	nop
+.L4:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC1, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+.L5:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-44)"
+	.section	.note.GNU-stack,"",@progbits
