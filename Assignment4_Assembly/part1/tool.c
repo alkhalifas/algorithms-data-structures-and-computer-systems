bbca41f88f6179c98ce16c8eb8615823ba8d3d1e
alkhalifas <salehesam@gmail.com>
2022-02-14 19:50:11 -0500

    Assignment4 - Reader now reading and printing

--- Assignment4_Assembly/part1/tool.c
+++ Assignment4_Assembly/part1/tool.c
@@ -1,6 +1,6 @@
 // Implement your cycle count tool here.
 #include <stdio.h>
-
+#include <stdlib.h>
 
 
 
@@ -12,6 +12,7 @@
 
 int main(int argc, char** argv){
     
+    // Set counts for each of the instructions
     int addCount = 0;
     int subCount = 0;
     int mulCount = 0;
@@ -22,26 +23,31 @@ int main(int argc, char** argv){
     int popCount = 0;
     int retCount = 0;
 
-
-
-
-
+    // Iterate over the args with argv[1] being the file of interest
     int i;
     for (i=0; i< argc; i++) {
         printf("argv[%d] is %s\n", i, argv[i]);
     }
+ 
+    // Open the file using argv[1]
+    FILE* myFile = fopen(argv[1], "r");
 
-    FILE myFile = fopen(argv[1], "r");
-
+    // Account for when file is missing or not found
     if(myFile == NULL) {
         fprintf(stderr, argv[1], " not found\n");
         exit(1);
     }
 
+    // Set buffer
     char buffer[255];
 
-    
+    fseek(myFile, 0, SEEK_SET);
+    while(fgets(buffer, 255, myFile)){
+        printf("LINE: %s", buffer);
 
+    }
+    
+    fclose(myFile);
 
 
     return 0;
