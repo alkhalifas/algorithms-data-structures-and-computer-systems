d808993ca43e912a7b399f3c5f9372ad8ba1fc6e
alkhalifas <salehesam@gmail.com>
2022-02-14 19:45:05 -0500

    Assignment4 - Added counts, args, file reader, still need to implement actual counter for lines

--- Assignment4_Assembly/part1/tool.c
+++ Assignment4_Assembly/part1/tool.c
@@ -1,6 +1,51 @@
 // Implement your cycle count tool here.
+#include <stdio.h>
 
 
+
+
+
+
+
+
+
+
+int main(int argc, char** argv){
+    
+    int addCount = 0;
+    int subCount = 0;
+    int mulCount = 0;
+    int divCount = 0;
+    int movCount = 0;
+    int leaCount = 0;
+    int pushCount = 0;
+    int popCount = 0;
+    int retCount = 0;
+
+
+
+
+
+    int i;
+    for (i=0; i< argc; i++) {
+        printf("argv[%d] is %s\n", i, argv[i]);
+    }
+
+    FILE myFile = fopen(argv[1], "r");
+
+    if(myFile == NULL) {
+        fprintf(stderr, argv[1], " not found\n");
+        exit(1);
+    }
+
+    char buffer[255];
+
+    
+
+
+
+    return 0;
+}
 
 
 
