8b4c3df0d8832e1d65470fe42d4e0a0083607ed5
alkhalifas <salehesam@gmail.com>
2022-03-01 19:49:03 -0500

    Lab6 almost done, still seeing an error about 86_64

--- Lab6_Processes/Part1/myprogram.c
+++ Lab6_Processes/Part1/myprogram.c
@@ -3,6 +3,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     // Terminate after 180 seconds
@@ -14,18 +15,17 @@ int main() {
     char* myArgv3[16];
     char* myArgv4[16];
 
-    myArgv1[0] = '/bin/ls';
+    myArgv1[0] = "/bin/ls";
     myArgv1[1] = "'-l'";
     myArgv1[2] = NULL;
 
-    myArgv2[0] = '/bin/echo';
+    myArgv2[0] = "/bin/echo";
     myArgv2[1] = "'CS is Awesome!'";
     myArgv2[2] = NULL;
 
     myArgv3[0] = NULL;
 
-    char** myCommands[];
-    myCommands = {myArgv1, myArgv2, myArgv3};
+    char** myCommands[] = {myArgv1, myArgv2, myArgv3};
 
     printf("#################################");
     printf("############ Starting ###########");
