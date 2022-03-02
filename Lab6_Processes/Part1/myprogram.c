f06f823fb23b2f6f48b06d3155fb9f885475caa7
alkhalifas <salehesam@gmail.com>
2022-03-01 20:26:19 -0500

    Lab 6 completed

--- Lab6_Processes/Part1/myprogram.c
+++ Lab6_Processes/Part1/myprogram.c
@@ -15,35 +15,44 @@ int main() {
     char* myArgv3[16];
     char* myArgv4[16];
 
+    // Set up first command
     myArgv1[0] = "/bin/ls";
-    myArgv1[1] = "'-l'";
-    myArgv1[2] = NULL;
+    myArgv1[1] = NULL;
 
+    // Set up second command
     myArgv2[0] = "/bin/echo";
-    myArgv2[1] = "'CS is Awesome!'";
+    myArgv2[1] = "'I love Lady Gaga!'";
     myArgv2[2] = NULL;
 
-    myArgv3[0] = NULL;
+    // Set up third command
+    myArgv3[0] = "/bin/nl";
+    myArgv3[1] = "example5.c";
+    myArgv3[2] = NULL;
 
+    // Create list of commands
     char** myCommands[] = {myArgv1, myArgv2, myArgv3};
 
-    printf("#################################");
-    printf("############ Starting ###########");
-    printf("#################################");
+    // Print results
+    printf("###############################################################\n");
+    printf("############################# Starting ########################\n");
+    printf("###############################################################\n");
 
+    // Iterate over list of 3 commands
     int i;
     for(i = 0; i < 3; i++){
+        // Add fork conditional
         if(fork() == 0) {
-            printf("## RUNNING PROCESS # = %d\n", i);
+            printf("###################### RUNNING PROCESS %d #####################\n", i);
+            // Call execute program command
             execve(myCommands[i][0], myCommands[i], NULL);
             exit(1);
         } else {
             wait(NULL);
         }
     }
-    printf("#################################");
-    printf("########### Completed ###########");
-    printf("#################################");
+    printf("###############################################################\n");
+    printf("############################ Completed ########################\n");
+    printf("###############################################################\n");
   
     return 0;
 }
