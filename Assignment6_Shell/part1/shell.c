1c95e2319dadaa11d54ced89d728896e5a19a8de
Saleh Alkhalifa <salehesam@gmail.com>
2022-03-03 16:22:32 -0800

    Error compiling

--- Assignment6_Shell/part1/shell.c
+++ Assignment6_Shell/part1/shell.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <signal.h>
 
 // Define the buffer
 #define BUFFER 80
 
 // Signal handler for when users click CTRL+C
 void sigint_handler(int signal){
-	write(1, "\nmini-shell terminated!\n", 35)
+	write(1, "\nmini-shell terminated!\n", 35);
 }	
 
 
@@ -22,7 +22,12 @@ int main(){
               // This will terminate your shell after 180 seconds,
               // and is useful in the case that you accidently create a 'fork bomb'
   
-  signal(SIGINT, signalHandler)
+  signal(SIGINT, sigint_handler);
+  printf("You can only terminate by pressing CTRL+C\n");
+  while(1){
+	  printf("Running forever!\n");
+	  sleep(1);
+  }
   
   return 0;
 }
