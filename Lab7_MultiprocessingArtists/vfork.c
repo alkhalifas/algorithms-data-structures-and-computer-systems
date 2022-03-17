089ee97e14a36b6634706496f891f208a3b57dae
alkhalifas <salehesam@gmail.com>
2022-03-16 21:53:03 -0400

    Working on Lab7 - vfork.c

--- Lab7_MultiprocessingArtists/vfork.c
+++ Lab7_MultiprocessingArtists/vfork.c
@@ -6,18 +6,28 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-void paint(int i) {
+int colors[64][64*3];
+
+void paint(int workID) {
     
     printf("Artist %d is painting\n", workID);
 
+    int i;
+    for(i = 0; i < 64*3; i++){
+        colors[workID][i] = workID;
 }
 
 int main() {
 
     int numberOfArtists = 8;
-
+ 
+    pid_t pid;
+    
     int i;
     for(i = 0; i < numberofArtists; i++){
         
         // Perform a fork
-        pid = fork()
+        pid = vfork()
+
+        if(pid == 0) {
+            
