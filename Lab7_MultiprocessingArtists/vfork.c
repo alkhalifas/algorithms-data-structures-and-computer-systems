3520190cbf0526f74d83e7880b8586056b6f57be
alkhalifas <salehesam@gmail.com>
2022-03-16 21:46:54 -0400

    Working on Lab7 - vfork.c

--- Lab7_MultiprocessingArtists/vfork.c
+++ Lab7_MultiprocessingArtists/vfork.c
@@ -1,2 +1,23 @@
 // Implement your part 1 solution here
 // gcc vfork.c -o vfork
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void paint(int i) {
+    
+    printf("Artist %d is painting\n", workID);
+
+}
+
+int main() {
+
+    int numberOfArtists = 8;
+
+    int i;
+    for(i = 0; i < numberofArtists; i++){
+        
+        // Perform a fork
+        pid = fork()
