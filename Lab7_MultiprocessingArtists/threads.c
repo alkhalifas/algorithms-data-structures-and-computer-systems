8d3171cf29ca448350e6967f5e7e141fe1a80d49
alkhalifas <salehesam@gmail.com>
2022-03-19 16:22:26 -0400

    Lab7 Part 2 threads working

--- Lab7_MultiprocessingArtists/threads.c
+++ Lab7_MultiprocessingArtists/threads.c
@@ -8,9 +8,7 @@
 # define NTHREADS 64
 
 int counter = 0;
-
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-
 char COLORS[64][64*3];
 
 // Function to save image
@@ -18,16 +16,25 @@ void savePPM() {
 
 }
 
-// Function to create thread
-void *thread(void *vargp) {
-    pthread_mutex_lock(&mutex1);
-    counter = counter + 1;
+// Paint function
+void paint(int workID) {
+    printf("Artist %d is painting\n", workID);
 
     int i;
-    // Iterate over COLORS and set counter
-    for(i = 0; i < 64*3; i++) {
-        COLORS[counter][i] = counter;
-    } 
+    // Iterate and assign value
+    for(i = 0; i < 64*3; i++){
+        COLORS[workID][i] = rand() % 256;
+    }
+}
+
+// Function to create thread
+void *thread(void *argp) {
+
+    // Lock
+    pthread_mutex_lock(&mutex1);
+
+    // Increment
+    counter = counter + 1;
 
     // Unlock
     pthread_mutex_unlock(&mutex1);
@@ -36,20 +43,28 @@ void *thread(void *vargp) {
 
 // Main Function
 int main() {
+    // Manage tids
     pthread_t tids[NTHREADS];
     printf("Start Counter: %d\n", counter);
     
     
+    printf("|------------- Starting Creating -------------|\n");
+
     int i;
+    // Iterate and create threads
     for(i = 0; i < NTHREADS; i++) {
-        pthread_t THREAD;
-        tids[i] = THREAD;
-        pthread_create(&THREAD, NULL, thread, NULL);
+        pthread_create(&tids[i], NULL, thread, NULL);
+        printf("Created thread %d --> %d\n", i, tids[i]);
+        // Paint the image
+        paint(i);
     }
 
-    int j;
-    for (j = 0; j < NTHREADS; j++) {
-        pthread_join(tids[j], NULL);
+    printf("|------------- Moving to Joining -------------|\n");
+    
+    // Iterate and join threads
+    for (i = 0; i < NTHREADS; i++) {
+        printf("Joined thread %d --> %d\n", i, tids[i]);
+        pthread_join(tids[i], NULL);
     }
 
     printf("Final counter: %d\n", counter);
