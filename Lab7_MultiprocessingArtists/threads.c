d744ff2cc97476b4dbc23f266784395273ffb249
alkhalifas <salehesam@gmail.com>
2022-03-19 15:24:38 -0400

    Working on Lab7 part 2

--- Lab7_MultiprocessingArtists/threads.c
+++ Lab7_MultiprocessingArtists/threads.c
@@ -5,30 +5,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 # define NTHREADS 64
+
+int counter = 0;
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 char COLORS[64][64*3];
 
-
-
+// Function to save image
 void savePPM() {
 
 }
 
+// Function to create thread
+void *thread(void *vargp) {
+    pthread_mutex_lock(&mutex1);
+    counter = counter + 1;
 
-void *thread() {
+    int i;
+    // Iterate over COLORS and set
+    
 
+    // Unlock
+    pthread_mutex_unlock(&mutex1);
+    return NULL;
 }
 
-
-void paint() {
-
-}
-
-
+// Main Function
 int main() {
-
+    pthread_t tids[NTHREADS];
+    printf("Counter starts at: %d\n", counter);
 
 
     return 0;
