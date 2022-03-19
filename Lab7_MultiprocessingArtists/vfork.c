09528c201be0abb2956ee3cc63a57da5bf424106
alkhalifas <salehesam@gmail.com>
2022-03-19 12:10:16 -0400

    added save, still not working

--- Lab7_MultiprocessingArtists/vfork.c
+++ Lab7_MultiprocessingArtists/vfork.c
@@ -6,37 +6,78 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// Canvas represented by 64 rows x 64 columns, with
+// each pixel storing 3 color components (RGB)
 int colors[64][64*3];
 
+// Paint function that takes a workID and paints along
+// that IDs row while iterating over the columns
 void paint(int workID) {
     
+    // Print artistID
     printf("Artist %d is painting\n", workID);
 
+    // Iterate over the columns, assign value
     int i;
     for(i = 0; i < 64*3; i++){
         colors[workID][i] = workID;
+    }
+}
+
+void savePPM(int colors){
+
+    FILE *fp;
+    fp = fopen("vfork.ppm", "w+");
+    fputs("P3\n", fp);
+    fputs("64 64\n", fp);
+    fputs("255\n", fp);
+    
+    int i;
+    int j;
+
+    for(i = 0; i < 64; i++){
+        for(j = 0; j < 64*3; j++){
+            pfintf(fp, "%d", colors[i][j]);
+            fputs(" ", fp);
+        }
+        fputs("\n", fp);
+    }
+    fclose(fp);
+
 }
 
 int main() {
 
+    // Select number of artists, which correspond to
+    // child processes in the program
     int numberOfArtists = 8;
  
+    // Variable to store the process IDs
     pid_t pid;
     
+    // Iterate over the number of artists or processes 
+    // selected and fork
     int i;
-    for(i = 0; i < numberofArtists; i++){
+    for(i = 0; i < numberOfArtists; i++){
         
         // Perform a fork
-        pid = vfork()
-
+        pid = vfork();
+        
+        // Print PID
+        printf("PID is: %d\n", pid);
+        
+        // Call paint then exit
         if(pid == 0) {
             paint(i);
-            exit(0)
+            exit(0);
         }
+       
         printf("Child process created: %d\n", pid);
     }
     printf("Masterpiece(vfork.ppm) is being assembled\n");
     
+    save(colors);
+
     return 0;
 }
             
