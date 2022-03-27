8cc9effd48abcb01b6379e14c5cee087b96070c4
alkhalifas <salehesam@gmail.com>
2022-03-27 15:26:46 -0400

    Assignment8 - Added swap function

--- Assignment8_Sorting/sort.c
+++ Assignment8_Sorting/sort.c
@@ -9,6 +9,10 @@
 // Input: The 'address of' an index into an array for positions in an array.
 void swap(int* a, int* b){
     // TODO: Swap two integers in an array.
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
 }
 
 // Provided below is a sort function. We have also
